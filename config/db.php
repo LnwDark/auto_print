e00d9f2fa77ce53055ca288085220e5eca882411
LnwDark <04061surichai@gmail.com>
2018-10-15 23:26:50 +0700

    Update db.php

--- config/db.php
+++ config/db.php
@@ -2,15 +2,10 @@
 
 return [
 //    'class' => 'yii\db\Connection',
-//    'dsn' => 'pgsql:host=localhost;port=5432;dbname=order_printer',
-//    'username' => 'postgres',
-//    'password' => 'lnwdark',
+//    'dsn' => 'pgsql:host=localhost;port=xxxx;dbname=xxxxx',
+//    'username' => 'xxxx',
+//    'password' => 'xxxx',
 //    'charset' => 'utf8',
-            'class' => 'yii\db\Connection',
-            'dsn' => 'pgsql:host=139.59.240.55;port=5432;dbname=order_printer',
-            'username' => 'postgres',
-            'password' => 'nextschoolnham2017',
-            'charset' => 'utf8',
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
